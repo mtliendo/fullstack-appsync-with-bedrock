f661f82918e66c0978d0d70ebe6f602a70a38eb9
focusotter <mtliendo@focusotter.com>
2023-10-13 12:03:01 -0500

    update with build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { config } from '@/aws-exports'
 import type { AppProps } from 'next/app'
 
 Amplify.configure(config)
-
+console.log('first')
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Authenticator.Provider>
